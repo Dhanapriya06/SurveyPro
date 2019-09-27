8898b867807813751578e0db6dfb0e33ae89d510
Dhanapriya <dhanapriya678@gmail.com>
2019-09-27 21:59:38 +0530

    first commit

---
+++ src/main/java/com/makeathon/surveypro/SurveyproApplication.java
@@ -0,0 +1,13 @@
+package com.makeathon.surveypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyproApplication.class, args);
+	}
+
+}
